bbe9cd0de5ba0b34a4ddf006bf9e3f7dc25161dc
Adrien Chabod <adrien.chabod@iriseden.fr>
2023-10-26 19:18:38 +0200

    Feature to choose TAIGA_EVENTS_RABBITMQ_HOST (in case of Kubernetes).

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -12,6 +12,11 @@ if [ "${1#-}" != "${1}" ] || [ -z "$(command -v "${1}")" ]; then
   set -- node "$@"
 fi
 
+if [ -z "$TAIGA_EVENTS_RABBITMQ_HOST" ]; then
+  echo "TAIGA_EVENTS_RABBITMQ_HOST is not set. Using default value: taiga-events-rabbitmq"
+  export TAIGA_EVENTS_RABBITMQ_HOST=taiga-events-rabbitmq
+fi
+
 envsubst < /taiga-events/docker/env.template \
          > /taiga-events/.env
 
